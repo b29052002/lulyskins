0e4641e7f0395507e33304a755fcf40923578059
hivemkt <email.brunoalbuquerque@gmail.com>
2026-02-08 20:46:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
-window.CONFIG = null;
-
-window.loadConfig = async function () {
-    const res = await fetch('/.netlify/functions/config');
-    window.CONFIG = await res.json();
-};
+window.CONFIG_PROMISE = fetch('/.netlify/functions/config')
+    .then(res => {
+        if (!res.ok) throw new Error("Erro ao carregar config");
+        return res.json();
+    })
+    .then(data => {
+        window.CONFIG = data;
+        console.log("CONFIG carregado:", data);
+        return data;
+    })
+    .catch(err => {
+        console.error("Falha ao carregar config:", err);
+        window.CONFIG = {};
+    });
